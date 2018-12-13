4ee3e6f472e725273cb327e41c800a0d32163f6b
KageKirin <kagekirin@gmail.com>
2018-12-13 18:08:32 +0900

    added rudimentary schema and template loading to js2fbs.py

--- generator/js2fbs.py
+++ generator/js2fbs.py
@@ -5,16 +5,45 @@ import sys
 import re
 import string
 import argparse
+import json
+import jinja2
 from pathlib import Path
 
 ###----
 
-
+jjLoader = None
+jjEnvironment = None
 
 ###----
 
+def loadFileFromRef(dirs, filename, loader):
+    f = list(filter(lambda d: d.joinpath(filename).is_file(), dirs))
+    if len(f) > 1:
+        return loader(f[0])
+    return None
+
+def loadSchema(filename):
+    import codecs
+    with codecs.open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def loadTemplate(filename):
+    return jjEnvironment.get_template(filename)
+
+def loadSchemaFromRef(dirs, filename):
+    return loadFileFromRef(dirs, filename, loadSchema)
+
+###----
+
+
+
 def main(args):
     print(args)
+    s = loadSchema(args.schema)
+    print(s)
+
+    t = loadTemplate(str(args.template.name))
+    print(t.render(obj=s))
 
 ###----
 
@@ -29,11 +58,15 @@ if __name__ == '__main__':
     if not args.include_dir:
         args.include_dir = [args.schema.parent]
     else:
-        args.include_dir.insert(0, args.schema.parent)
+        args.include_dir.append(args.schema.parent)
     if not args.template_dir:
         args.template_dir = [args.template.parent]
     else:
-        args.template_dir.insert(0, args.template.parent)
+        args.template_dir.append(args.template.parent)
+    
+    #jjLoader = jinja2.FileSystemLoader(list(map(lambda x: str(x.resolve()), args.template_dir)))
+    jjLoader = jinja2.FileSystemLoader(args.template_dir)
+    jjEnvironment = jinja2.Environment(loader=jjLoader)
     main(args)
 
 ###----
